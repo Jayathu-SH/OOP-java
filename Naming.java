eef4c7b88b05aeaee19942d566183566dd5aa59e
jayathu sanka <shjayathusanka@gmail.com>
2025-01-01 17:39:15 +0530

    Camel Casing Up & Down

---
+++ Naming.java
@@ -0,0 +1,17 @@
+public class Naming {
+    
+    public static void main(String[] args) {
+        
+
+
+
+    }
+}
+
+// Camel casing          Up and down
+
+//class and interface - Calc
+//variable and method - marks,show()
+//constants - PIE, BRAND
+
+//much more readable above this
---
+++ Super.java
@@ -0,0 +1,20 @@
+class A 
+{
+
+}
+
+class B extends A 
+{
+    public B()
+    {
+        System.out.println("in B");
+    }
+
+}
+
+public class Super {
+    public static void main(String[] args) {
+        
+        B obj = new B();
+    }
+}
