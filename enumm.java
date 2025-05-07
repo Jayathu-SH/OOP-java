a9d02504a449794e4dd349f669e84b6c8735d3fd
jayathu sanka <shjayathusanka@gmail.com>
2025-05-07 23:01:09 +0530

    enum switch if else

---
+++ enumm.java
@@ -0,0 +1,38 @@
+
+enum Status{
+    Running, Failed, Pending, Success;
+}
+
+public class enumm {
+    public static void main(String[] args) {
+        
+        Status s = Status.Success;
+
+        switch(s)
+        {
+            case Running:
+            System.out.println("All Good");
+            break;
+
+            case Failed:
+            System.out.println("Try Again");
+            break;
+
+            case Pending:
+            System.out.println("Please wait");
+            break;
+
+            default:
+            System.out.println("Done");
+        }
+        
+        // if(s==Status.Running)
+        // System.out.println("All Good");
+        // else if(s==Status.Failed)
+        // System.out.println("Try Again");
+        // else if(s==Status.Pending)
+        // System.out.println("Please wait");
+        // else
+        // System.out.println("Done");
+    }
+}
