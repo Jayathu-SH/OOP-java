1095df98f021fd70405a8a27f18eb8b40f5f92d7
jayathu sanka <shjayathusanka@gmail.com>
2025-05-07 18:12:04 +0530

    abstrct anonymus inner

--- Anonymousinnerclz.java
+++ Anonymousinnerclz.java
@@ -1,22 +1,20 @@
-class A{
-    public void show()
-    {
-        System.out.println("in A show");
-    }
+abstract  class A{
+    public abstract void show();
+    
 }
+
 
 
 
 public class Anonymousinnerclz {
     public static void main(String[] args) {
-        A obj = new A()
-        {
-            public void show()
-            {
-                System.out.println("in new show");
-            }
-        };
-        obj.show();
+       A obj = new A()
+       {
+                public void show(){
+                    System.out.println("in new show");
+                }
+       };
+       obj.show();
     }
 }
 
